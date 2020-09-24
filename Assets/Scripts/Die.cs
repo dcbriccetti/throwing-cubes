e3f20c627faadfbabe8905b633a2fdc9e03cb2d1
Dave Briccetti <daveb@davebsoft.com>
2020-09-24 14:06:17 -0700

    Create first version of work-in-progress game

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Die : MonoBehaviour {
+    public Texture[] texture;
+    private Rigidbody rb;
+
+    void Start() {
+        TextureOneFace();
+        Throw();
+    }
+
+    private void Throw() {
+        rb = transform.GetComponent<Rigidbody>();
+        float R() => Random.Range(-15000, 15000);
+        transform.Translate(Random.Range(-15f, 15f), Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        rb.velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-7f, -3f), Random.Range(30f, 50f));
+        rb.angularVelocity = new Vector3(R(), R(), R());
+    }
+
+    private void TextureOneFace() {
+        var mr = transform.GetComponent<MeshRenderer>();
+        var facesMaterials = mr.materials.Where(mat => mat.name != "Base (Instance)").ToArray();
+        var selMat = facesMaterials[Random.Range(0, facesMaterials.Count())];
+        selMat.mainTexture = texture[0];
+    }
+}
---
+++ Assets/Scripts/Thrower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Thrower : MonoBehaviour {
+    public Transform diePrefab;
+
+    private void Update() {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            Instantiate(diePrefab);
+            
+        }
+    }
+}
